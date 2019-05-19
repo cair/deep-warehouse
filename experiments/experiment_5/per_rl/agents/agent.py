Fixed a sleezey "bug" or "missconception" about GAE and general calculation of returns... it works quite well now, but i suppose it could work better?

experiments/experiment_5/per_rl/agents/agent.py
@@ -96,7 +96,8 @@ class Agent:
         self.metrics = Metrics(self)
         self.data = dict()  # Keeps track of all data per iteration. Resets after train()
         self.losses = dict()
-        self.preprocessors = OrderedDict()
+        self.batch_preprocessors = OrderedDict()
+        self.mb_preprocessors = OrderedDict()
 
         self.metrics.text("hyperparameters", tf.convert_to_tensor(utils.hyperparameters_to_table(self.args)))
 
@@ -115,17 +116,29 @@ class Agent:
         self.obs = None  # Last seen observation
         self.epoch = 0
 
-    def clear_preprocessors(self):
-        self.preprocessors.clear()
+    def clear_batch_preprocessors(self):
+        self.batch_preprocessors.clear()
 
-    def add_preprocessor(self, name, fn):
-        if name in self.preprocessors:
-            del self.preprocessors[name]
+    def add_batch_preprocessor(self, name, fn):
+        if name in self.batch_preprocessors:
+            del self.batch_preprocessors[name]
 
-        self.preprocessors[name] = fn
+        self.batch_preprocessors[name] = fn
 
-    def remove_preprocessor(self, name):
-        del self.preprocessors[name]
+    def remove_batch_preprocessor(self, name):
+        del self.batch_preprocessors[name]
+
+    def clear_mb_preprocessors(self):
+        self.mb_preprocessors.clear()
+
+    def add_mb_preprocessor(self, name, fn):
+        if name in self.mb_preprocessors:
+            del self.mb_preprocessors[name]
+
+        self.mb_preprocessors[name] = fn
+
+    def remove_mb_preprocessor(self, name):
+        del self.mb_preprocessors[name]
 
     def add_loss(self, name, lambda_fn, tb_text=None):
         self.losses[name] = lambda_fn
@@ -173,9 +186,9 @@ class Agent:
 
         """Metrics update."""
         self.metrics.add("steps", 1, ["sum", "sum_mean"], None, episode=True, epoch=True, total=True)
-        self.metrics.add("reward", kwargs["reward"], ["sum", "sum_mean"], None, episode=True, epoch=False, total=True)
+        self.metrics.add("reward", kwargs["rewards"], ["sum", "sum_mean"], None, episode=True, epoch=False, total=True)
 
-        if kwargs["terminal"]:
+        if kwargs["terminals"]:
             self.metrics.done(episode=True)
             self.metrics.summarize(["reward", "steps"])
 
@@ -203,6 +216,8 @@ class Agent:
                 # Do prediction using current slave policy, add this to the kwargs term.
                 kwargs.update(policy_train(**kwargs))
 
+                # Do preprossessing of mini-batch data
+                self._preprocessing(kwargs, mb=True)
                 # Run all loss functions
                 for loss_name, loss_fn in self.losses.items():
 
@@ -236,10 +251,11 @@ class Agent:
 
         return np.asarray(losses)
 
-    def _preprocessing(self, batch, **kwargs):
+    def _preprocessing(self, batch, mb=False, **kwargs):
+        preprocess_fns = self.mb_preprocessors if mb else self.batch_preprocessors
 
         # Preprocessing of data prior to training
-        for preprocess_name, preprocess_fn in self.preprocessors.items():
+        for preprocess_name, preprocess_fn in preprocess_fns.items():
             preprocess_res = preprocess_fn(**batch, **kwargs)
 
             if isinstance(preprocess_res, dict):

experiments/experiment_5/per_rl/agents/configuration/defaults.py
@@ -49,14 +49,14 @@ A2C = dict(
 PPO = dict(
 
     # Generalized Advantage Function # TODO
-    gae=False,  #
-    gae_lambda=0.95,
+    gae=True,  #
+    gae_lambda=1.0,
 
     # Returns
     gamma=0.99,
 
     # Policy coefficients
-    epsilon=0.3,  # Policy clipping factor
+    epsilon=0.2,  # Policy clipping factor
     kl_coef=0.2,  # TODO
     kl_target=0.01,  # TODO
     entropy_coef=0.01,  # Entropy should be 0.0 for continous action spaces.  # TODO
@@ -81,18 +81,18 @@ PPO = dict(
     # Policy definition
     policy=lambda agent: PPOPolicy(
         agent=agent,
-        #optimizer=dict(
-        #    policy=tf.keras.optimizers.Adam(lr=0.0007),
-        #    value=tf.keras.optimizers.Adam(lr=0.0007),
-        #)
-        optimizer=tf.keras.optimizers.Adam(lr=0.0007)
+        optimizer=dict(
+            policy=tf.keras.optimizers.Adam(lr=0.0007),
+            value=tf.keras.optimizers.Adam(lr=0.0007),
+        )
+        #optimizer=tf.keras.optimizers.Adam(lr=0.0007)
     ),
 
     # Policy update settings
     policy_update=dict(
         double=True,
         n_trainers=1,
-        interval=5,
+        interval=2,
         strategy="copy",  # copy, mean  # TODO wierd
         type="weights"  # weights, gradients  # TODO wierd
     )

experiments/experiment_5/per_rl/agents/configuration/models.py
@@ -288,7 +288,7 @@ class PPOPolicy(Policy):
         self.v_4 = tf.keras.layers.Dense(64, activation="tanh", dtype=self.agent.dtype)
         self.action_value = tf.keras.layers.Dense(1,
                                                   activation="linear",
-                                                  name="action_value",
+                                                  name="values",
                                                   dtype=self.agent.dtype)
 
     def call(self, inputs, **kwargs):
@@ -311,7 +311,7 @@ class PPOPolicy(Policy):
 
         return {
             "logits": policy_logits,
-            "action_value": tf.squeeze(action_value)
+            "values": tf.squeeze(action_value)
         }
 
 

experiments/experiment_5/per_rl/agents/ppo.py
@@ -17,6 +17,8 @@
 # DO SOME https://github.com/ray-project/ray/blob/master/python/ray/rllib/agents/ppo/ppo.py
 from scipy.signal import lfilter
 import tensorflow as tf
+
+from experiments.experiment_5.per_rl import utils
 from experiments.experiment_5.per_rl.agents.a2c import A2C
 from experiments.experiment_5.per_rl.agents.agent import Agent, DecoratedAgent
 from experiments.experiment_5.per_rl.agents.configuration import defaults
@@ -49,12 +51,13 @@ class PPO(Agent):
     def __init__(self, **kwargs):
         super(PPO, self).__init__(**kwargs)
 
-        self.add_preprocessor("advantage", self.advantage)
+        self.add_batch_preprocessor("advantages", self.advantages)
+        self.add_mb_preprocessor("advantages", self.mb_advantages)
 
         self.add_loss("policy_loss", self.policy_loss, "Policy loss of PPO")
         self.add_loss("value_loss", self.value_loss, "Action loss of PPO")
         self.add_loss("entropy_loss", self.entropy_loss, "Action loss of PPO")
-    # TODO make a way to adopt this from other classes i.e reinforce.
+        # TODO make a way to adopt this from other classes i.e reinforce.
 
     def _predict(self, inputs):
         pred = super()._predict(inputs)
@@ -62,46 +65,45 @@ class PPO(Agent):
         self.data["action"] = tf.one_hot(action, self.action_space)
         return action.numpy()
 
-    def value_loss(self, old_action_value, action_value, returns, **kwargs):
+    def value_loss(self, old_values, values, returns, **kwargs):
 
         if self.args["vf_clipping"]:
             v_pred_clipped = \
-                old_action_value + tf.clip_by_value(
-                    action_value - old_action_value,
+                old_values + tf.clip_by_value(
+                    values - old_values,
                     -self.args["vf_clip_param"],
                     self.args["vf_clip_param"]
                 )
 
-            vf_losses_1 = tf.square(action_value - returns)
+            vf_losses_1 = tf.square(values - returns)
             vf_losses_2 = tf.square(v_pred_clipped - returns)
             vf_loss = tf.reduce_mean(tf.maximum(vf_losses_1, vf_losses_2))
 
             return self.args["vf_coeff"] * vf_loss
         else:
-            return tf.losses.mean_squared_error(returns, action_value) * self.args["vf_coeff"]
+            return tf.losses.mean_squared_error(returns, values) * self.args["vf_coeff"]
 
-    def policy_loss(self, logits, old_logits, action, advantage, **kwargs):
+
+    def policy_loss(self, logits, old_logits, action, advantages, **kwargs):
 
         neglogpac_old = tf.reduce_sum(tf.math.log_softmax(old_logits, axis=1) * action, axis=1)
         neglogpac_new = tf.reduce_sum(tf.math.log_softmax(logits, axis=1) * action, axis=1)
 
         ratios = tf.exp(neglogpac_new - tf.stop_gradient(neglogpac_old))
 
-        surr1 = ratios * advantage
+        surr1 = ratios * advantages
         surr2 = tf.clip_by_value(ratios, 1.0 - self.args["epsilon"], 1.0 + self.args["epsilon"])
 
         # Metrics
-        approxkl = .5 * tf.reduce_mean(tf.square(neglogpac_new - neglogpac_old))
+        #approxkl = .5 * tf.reduce_mean(tf.square(neglogpac_new - neglogpac_old))
+        #self.metrics.add("approxkl", approxkl, ["mean"], "train", epoch=True)
+
         clipfrac = tf.reduce_mean(tf.cast(tf.greater(tf.abs(ratios - 1.0), self.args["epsilon"]), dtype=tf.float64))
-
-        self.metrics.add("approxkl", approxkl, ["mean"], "train", epoch=True)
         self.metrics.add("clipfrac", clipfrac, ["mean"], "train", epoch=True)
-
 
         return -tf.reduce_mean(tf.minimum(surr1, surr2))
 
     def entropy_loss(self, logits, **kwargs):
-        #entropy_loss = -tf.reduce_mean(tf.losses.categorical_crossentropy(logits, logits, from_logits=True) * self.entropy_coef)
 
         log_prob = tf.math.softmax(logits)
         entropy = self.args["entropy_coef"] * tf.reduce_mean(tf.reduce_sum(log_prob * tf.math.log(log_prob), axis=1))
@@ -110,69 +112,52 @@ class PPO(Agent):
     def discount(self, x, gamma):
         return lfilter([1], [1, -gamma], x[::-1], axis=0)[::-1]
 
-    def generalized_advantage_estimation(self, old_action_value, policy, obs1, reward, terminal, **kwargs):
-        """V_1 = np.concatenate((old_action_value[1:], [policy(obs1[-1:])["action_value"]]))
-        V = old_action_value
-        T_1 = np.concatenate((terminal[1:], [0]))
 
-        advantage = self.discount(
-            reward +
-            self.args["gamma"] * V_1 * (1 - T_1) -
-            V, self.args["gamma"] * self.args["gae_lambda"]
-        )
+    def mb_advantages(self, returns, values, old_values, **kwargs):
 
-        td_lambda_return = advantage + V
-        advantage = (advantage - advantage.mean()) / np.maximum(advantage.std(), 1e-6)
+        self.metrics.add(
+            "explained-variance",
+            utils.explained_variance(old_values, returns), ["mean"], "loss", epoch=True, total=True)
 
-        return dict(
-            returns=np.asarray(td_lambda_return),
-            advantage=np.asarray(advantage)
-        )"""
+        # Returns = R + yV(s')
+        advantage = np.asarray(returns - values)
+        # Normalize the advantages
+        advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-8)
+        return advantage
 
-        last_gae = 0
-        adv = np.zeros_like(reward)
-        ret = np.zeros_like(reward)
+    def generalized_advantage_estimation(self, old_values, policy, obs1, rewards, terminals, **kwargs):
+        V = np.concatenate((old_values, [policy(obs1[-1:])["values"]]))
+        terminal = np.concatenate((terminals, [0]))
+        gamma = self.args["gamma"]
+        lam = self.args["gae_lambda"]
+        adv = np.zeros_like(rewards)
+        lastgaelam = 0
+        for t in reversed(range(self.batch.counter)):
+            nextnonterminal = 1 - terminal[t+1]
+            delta = rewards[t] + gamma * (V[t+1] * nextnonterminal) - V[t]
+            adv[t] = lastgaelam = delta + gamma * lam * nextnonterminal * lastgaelam
 
-        V = old_action_value
-        V_1 = np.concatenate((old_action_value[1:], [policy(obs1[-1:])["action_value"]]))
-
-        for i in reversed(range(0, self.batch.counter)):
-            if terminal[i]:
-                delta = reward[i] - V[i]
-                last_gae = delta
-            else:
-                delta = reward[i] + self.args["gamma"] * V_1[i] - V[i]
-                last_gae = delta + self.args["gamma"] * self.args["gae_lambda"] * last_gae
-
-            adv[i] = last_gae
-            ret[i] = last_gae + V[i]
-
-        adv = (adv - adv.std()) / adv.mean()
+        returns = adv + old_values
 
         return dict(
-            advantage=adv,
-            returns=ret
+            returns=returns
         )
 
-    def discounted_returns(self, reward, terminal, old_action_value, **kwargs):
-        discounted_rewards = np.zeros_like(reward)
-        advantage = np.zeros_like(reward)
+    def discounted_returns(self, rewards, terminals, old_values, **kwargs):
+        discounted_rewards = np.zeros_like(rewards)
+        advantage = np.zeros_like(rewards)
         cum_r = 0
         for i in reversed(range(0, self.batch.counter)):
-            cum_r = reward[i] + cum_r * self.args["gamma"] * (1 - terminal[i])
+            cum_r = rewards[i] + cum_r * self.args["gamma"] * (1 - terminals[i])
 
             discounted_rewards[i] = cum_r
-            advantage[i] = cum_r - old_action_value[i]
-
-        discounted_rewards = (discounted_rewards - discounted_rewards.mean()) / (discounted_rewards.std() + 1e-9)
-        advantage = (advantage - advantage.mean()) / (advantage.std() + 1e-9)
+            advantage[i] = cum_r + old_values[i]
 
         return dict(
-            advantage=advantage,
             returns=discounted_rewards
         )
 
-    def advantage(self, **kwargs):
+    def advantages(self, **kwargs):
         if self.args["gae"]:
             return self.generalized_advantage_estimation(**kwargs)
         else: